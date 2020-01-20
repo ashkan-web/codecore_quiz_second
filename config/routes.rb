acabc56438eedfc59114e18dbc0114f61384cff1
ashkan <ashkanh.info@gmail.com>
2020-01-20 15:56:43 -0800

    user authentication was added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'welcome#home'
   # get '/about', to: 'ideas#about'
   resources :ideas
-  
+  resources :users , only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
 end
